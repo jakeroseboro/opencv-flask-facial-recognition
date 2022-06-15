6ad063cdcbb72d9143f33ac2eab43eb4c69f0383
jakeroseboro <57158408+jakeroseboro@users.noreply.github.com>
2022-06-14 19:42:17 -0500

    create templates for html stuff

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 
 from camera import webcam_face_detect
 import flask_cors
@@ -12,13 +12,23 @@ app.logger.setLevel(logging.ERROR)
 cors.init_app(app, resources={r"/*": {"origins": "*"}})
 
 
-@app.route("/viewer")
-def prediction_controller():
+@app.route("/facial_recognition")
+def facial_recognition():
     if len(sys.argv) >= 2:
         video_mode = sys.argv[1]
     else:
         video_mode = 0
     return Response(webcam_face_detect(video_mode), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route("/facial_recognition_demo")
+def facial_recognition_demo():
+    return render_template('facial_recognition.html')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
